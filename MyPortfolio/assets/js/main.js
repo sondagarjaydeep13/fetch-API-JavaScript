fffb948e5f1a887950eb9d2676044b0f59e82f9d
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-04-21 17:09:52 +0530

    add git

--- MyPortfolio/assets/js/main.js
+++ MyPortfolio/assets/js/main.js
@@ -1,10 +1,3 @@
-/**
- * Template Name: iPortfolio
- * Updated: Mar 10 2023 with Bootstrap v5.2.3
- * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function () {
   "use strict";
 
